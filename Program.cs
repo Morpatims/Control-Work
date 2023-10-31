8d0530a5edef62ba2b6bcbc25ae28cbd59388072
morpatims <kverteletski@mail.ru>
2023-10-31 14:45:46 +0300

    add solution task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива, разделяя их пробелами:");
+        string[] inputArray = Console.ReadLine().Split();
+
+        string[] newArray = new string[inputArray.Length];
+        int newIndex = 0;
+
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                newArray[newIndex] = item;
+                newIndex++;
+            }
+        }
+
+        string[] finalArray = new string[newIndex];
+        
+        Array.Copy(newArray, finalArray, newIndex);
+
+        Console.WriteLine("Итоговый массив:");
+        foreach (string item in finalArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
